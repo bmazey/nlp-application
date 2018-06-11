e6fd1fd2a814e1214ada7bf612a76911da22d1d0
Brandon <b.mazey@nyu.edu>
2018-06-11 19:59:11 -0400

    adding spring hateoas

--- src/main/java/org/columbia/service/LanguageService.java
+++ src/main/java/org/columbia/service/LanguageService.java
@@ -6,7 +6,6 @@ import org.columbia.entity.PartOfSpeech;
 import org.columbia.entity.Text;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
